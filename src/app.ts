f409fbe2514a22d13c756a07b3e5ad3619666e60
OneBigOwnage <niekvandenbos@hotmail.com>
2020-07-25 16:21:08 +0200

    Bring everything together in the app.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 import * as puppeteer from 'puppeteer';
+import WoongoedMakelaarsFetcher from './services/WoongoedMakelaarsFetcher';
+import * as dotenv from 'dotenv';
+import WhatsApp from './services/WhatsApp';
+
+dotenv.config();
+
 
 (async () => {
-    const browser = await puppeteer.launch();
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
 
-    const page = await browser.newPage();
-    await page.goto('https://www.woongoedmakelaars.nl/aanbod/woningaanbod/KRIMPEN%20AAN%20DEN%20IJSSEL/provincie-Zuid-Holland/');
+        const wgmFetcher = new WoongoedMakelaarsFetcher();
+        const homes = await wgmFetcher.homes(browser);
+        const whatsapp = new WhatsApp();
 
+        for (let i = 0; i < homes.length; i++) {
+            const house = homes[i];
 
+            await whatsapp.message(`We found a new house with status [${house.status}]: ${house.address}.\nThe house is listed for ${house.price}.\nFind out more at: ${house.url}`);
+
+            break;
+        }
+
+        await browser.close();
+    } catch (error) {
+        console.error('An error ocurred. More info below:\n\n', error);
+    }
 })();
