943b5d9179f195bc35db9698be804ef2bfdf85c8
OneBigOwnage <niekvandenbos@hotmail.com>
2020-08-17 19:38:20 +0200

    Properly implement homes and addresses.

--- src/contracts/HomeInformation.ts
+++ src/contracts/HomeInformation.ts
@@ -1,10 +1,47 @@
-import Status from "src/enums/Status";
+import StatusType from "src/enums/StatusType";
 import Realtor from "src/enums/Realtor";
 
-export default interface HomeInformation {
+export default interface LegacyHomeInformation {
     address: string;
     price: string;
-    status: Status;
+    status: StatusType;
+    url: string;
+    realtor: Realtor;
+}
+
+export interface Address {
+    googlePlaceID: string;
+    street: string;
+    number: string;
+    city: string;
+    zipcode: string;
+}
+
+interface Status {
+    startDate: Date | null;
+    endDate: Date | null;
+    type: StatusType;
+}
+
+interface RealtorObject {
+    price: number;
+    status: StatusType;
+    url: string;
+    realtor: Realtor;
+    previousStatusses: Array<Status>;
+}
+
+
+export class Home implements Address, RealtorObject {
+    googlePlaceID: string;
+    street: string;
+    number: string;
+    city: string;
+    zipcode: string;
+    price: number;
+    priceType: 'TransferTax' | 'RegisteredFreely';
+    status: StatusType;
+    previousStatusses: Array<Status>;
     url: string;
     realtor: Realtor;
 }
