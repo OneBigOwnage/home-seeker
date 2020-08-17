0c511c540c82e8b42e72dedd34b787d588e029f0
OneBigOwnage <niekvandenbos@hotmail.com>
2020-08-17 20:48:26 +0200

    Interim commit.

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,68 @@
 import * as puppeteer from 'puppeteer';
-import WoongoedMakelaarsFetcher from './services/WoongoedMakelaarsFetcher';
 import * as dotenv from 'dotenv';
-import WhatsApp from './services/WhatsApp';
+import * as Redis from 'async-redis';
+
+// import WoongoedMakelaarsFetcher from 'src/services/WoongoedMakelaarsFetcher';
+
+import WhatsApp from 'src/services/WhatsApp';
+import Maps from './services/Maps';
+import WoongoedMakelaarsFetcher from './services/WoongoedMakelaarsFetcher';
+// import WoongoedMakelaarsFetcher from './services/WoongoedMakelaarsFetcher';
+
 
 dotenv.config();
 
 
+// const redis = Redis.createClient({ host: 'redis' });
+
+// redis.on('error', console.error);
+
+// redis.set('foo', 'bar');
+// redis.get('foo', Redis.print);
+
+// redis.quit();
+
+
+
 (async () => {
-    try {
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+    const maps = new Maps(process.env.GCP_API_KEY);
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const fetcher = new WoongoedMakelaarsFetcher(browser, maps);
 
-        const wgmFetcher = new WoongoedMakelaarsFetcher();
-        const homes = await wgmFetcher.homes(browser);
-        const whatsapp = new WhatsApp();
-
-        for (let i = 0; i < homes.length; i++) {
-            const house = homes[i];
-
-            await whatsapp.message(`We found a new house with status [${house.status}]: ${house.address}.\nThe house is listed for ${house.price}.\nFind out more at: ${house.url}`);
-
-            break;
-        }
-
-        await browser.close();
-    } catch (error) {
-        console.error('An error ocurred. More info below:\n\n', error);
-    }
+    console.log(await fetcher.homes());
 })();
+
+
+// (async () => {
+//     const maps = new Maps(process.env.GCP_API_KEY);
+//     let placeID = await maps.findPlaceID('D albert schweitzer straat 18 Bergambacht');
+
+//     console.log(await maps.placeIDToAddress(placeID));
+// })();
+
+
+
+
+// (async () => {
+//     try {
+//         const browser = await puppeteer.launch({
+//             args: ['--no-sandbox', '--disable-setuid-sandbox']
+//         });
+
+//         const wgmFetcher = new WoongoedMakelaarsFetcher();
+//         const homes = await wgmFetcher.homes(browser);
+//         const whatsapp = new WhatsApp();
+
+//         for (let i = 0; i < homes.length; i++) {
+//             const house = homes[i];
+
+//             await whatsapp.message(`We found a new house with status [${house.status}]: ${house.address}.\nThe house is listed for ${house.price}.\nFind out more at: ${house.url}`);
+
+//             break;
+//         }
+
+//         await browser.close();
+//     } catch (error) {
+//         console.error('An error ocurred. More info below:\n\n', error);
+//     }
+// })();
