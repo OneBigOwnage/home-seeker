fa52258805a56fbe8521b616d968630c16da845e
OneBigOwnage <niekvandenbos@hotmail.com>
2020-08-21 22:43:50 +0200

    Turn database connection into a singleton.

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,8 @@
 import User from "./entities/User";
 import Realtor from "./entities/Realtor";
 import Home from "./entities/Home";
-import { ConnectionOptions, createConnection } from "typeorm";
+import { ConnectionOptions, createConnection, Connection } from "typeorm";
+import HomeStatus from "./entities/HomeStatus";
 
 export const dbOpts: ConnectionOptions = {
     type: 'mysql',
@@ -10,10 +11,17 @@ export const dbOpts: ConnectionOptions = {
     username: 'root',
     password: 'root',
     database: 'db',
-    entities: [ User, Realtor, Home ]
+    entities: [User, Realtor, Home, HomeStatus],
 };
 
-const database = async () => createConnection(dbOpts);
+let connection: Connection;
 
+const database = async () => {
+    if (connection) {
+        return connection;
+    }
+
+    return connection = await createConnection(dbOpts);
+}
 
 export default database;
