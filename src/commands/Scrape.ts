968f0c2396b1227f9f1bccb12521886de4b6638e
OneBigOwnage <niekvandenbos@hotmail.com>
2020-08-23 13:53:40 +0200

    Getting the connection string is now a function.
    This way, the process.env is accessed after it's been filled instead
    of before.

--- src/commands/Scrape.ts
+++ src/commands/Scrape.ts
@@ -5,14 +5,14 @@ import * as puppeteer from 'puppeteer';
 import ProducerService from "../rabbit/Producer";
 import WoongoedMakelaarsFetcher from "../services/WoongoedMakelaarsFetcher";
 
-const connectionString = `amqp://${process.env.RABBIT_USERNAME}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`;
+const connectionString = () => `amqp://${process.env.RABBIT_USERNAME}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`;
 const scrapedHomesQueue = 'SCRAPED_HOMES';
 
 const scrape = async () => {
     const redis = createClient({ host: 'redis' });
     const maps = new Maps(new Client(), redis, process.env.GCP_API_KEY)
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const producer = new ProducerService(connectionString);
+    const producer = new ProducerService(connectionString());
 
     const fetcher = new WoongoedMakelaarsFetcher(browser, maps);
 
